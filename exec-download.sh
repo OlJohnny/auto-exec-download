a52f8c99dbcedf9e0b1d2bdcbb544215ee27bef5
OlJohnny <Jonatwebhan@gmail.com>
2019-05-14 23:22:46 +0200

    added nice names

--- exec-download.sh
+++ exec-download.sh
@@ -67,90 +67,220 @@ mkdir ./tmp-aed
 
 ##### DOWNLOAD OF FILES #####
 ### winrar ###
+## download ##
 echo -e "\n<$(date +"%T")> Getting WinRar...\e[90m"
+# download exec
 wget --quiet --output-document=- https://www.winrar.de/downld.php | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '(http|https):\/\/winrar.*x64-[0-9]{3}d\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/WinRAR " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://www.winrar.de/downld.php | grep --extended-regexp --only-matching --ignore-case 'Aktuelle Version: WinRAR [0-9]\.[0-9]{1,2}' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/winrar-x64-*.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### cpu-z ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting CPU-Z...\e[90m"
+# get link to download page
 wget --quiet --output-document=- https://www.cpuid.com/softwares/cpu-z.html | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '/downloads/cpu-z/cpu-z_[0-9]{1}\.[0-9]{2}-en\.exe' | head --lines=1 > ./tmp-aed/.exec-work
 sed --in-place '1 i\https://www.cpuid.com' ./tmp-aed/.exec-work
 sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
-wget --quiet --output-document=- --input-file=./tmp-aed/.exec-work | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '(http|https)://download\.cpuid\.com/cpu-z/cpu-z_[0-9]\.[0-9]{2}-en\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
-rm ./tmp-aed/.exec-work
+# download exec
+wget --quiet --output-document=- --input-file=./tmp-aed/.exec-work | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '(http|https)://download\.cpuid\.com/cpu-z/cpu-z_[0-9]\.[0-9]{1,2}-en\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/CPU-Z " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- --input-file=./tmp-aed/.exec-work | grep --extended-regexp --only-matching --ignore-case 'cpu-z_[0-9]\.[0-9]{1,2}-en\.exe' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/cpu-z_*.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### hwmonitor ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting HWMonitor...\e[90m"
+# get link to download page
 wget --quiet --output-document=- https://www.cpuid.com/softwares/hwmonitor.html | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '/downloads/hwmonitor/hwmonitor_[0-9]{1}\.[0-9]{2}\.exe' | head --lines=1 > ./tmp-aed/.exec-work
 sed --in-place '1 i\https://www.cpuid.com' ./tmp-aed/.exec-work
 sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
+# download exec
 wget --quiet --output-document=- --input-file=./tmp-aed/.exec-work | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '(http|https)://download\.cpuid\.com/hwmonitor/hwmonitor_[0-9]\.[0-9]{2}\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
-rm ./tmp-aed/.exec-work
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/HWMonitor " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- --input-file=./tmp-aed/.exec-work | grep --extended-regexp --only-matching --ignore-case 'hwmonitor_[0-9]\.[0-9]{1,2}\.exe' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/hwmonitor_*.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### geforce experience ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting GeForce Experience...\e[90m"
+# get link to download page
 wget --quiet --output-document=- https://www.nvidia.de/object/geforce-experience-download-de.html | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '//de\.download\.nvidia\.com/GFE/GFEClient/.*\.exe' | head --lines=1 > ./tmp-aed/.exec-work
 sed --in-place '1 i\https:' ./tmp-aed/.exec-work
 sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
+# download exec
 wget --quiet --show-progress --input-file=./tmp-aed/.exec-work --directory-prefix=./tmp-aed/
-rm ./tmp-aed/.exec-work
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/GeForce Experience " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://www.nvidia.de/object/geforce-experience-download-de.html | grep --extended-regexp --only-matching --ignore-case 'Version:.*[0-9]\.[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,3}' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,3}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/GeForce_Experience_*.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### putty ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting PuTTY...\e[90m"
+# download exec
 wget --quiet --output-document=- https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching 'https://the\.earth\.li/~sgtatham/putty/latest/w64/putty-64bit-[0-9]\.[0-9]{2}-installer\.msi' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/PuTTY " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html | grep --extended-regexp --only-matching --ignore-case 'Currently this is [0-9]\.[0-9]{1,2}' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".msi" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/putty-64bit-*-installer.msi {}
+rm ./tmp-aed/.exec-*
 
 
 ### vlc ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting VLC...\e[90m"
+# get link to download page
 wget --quiet --output-document=- https://www.vlc.de/vlc_download_64bit.php | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '//files\.vlc\.de/vlc/vlc-[0-9]\.[0-9]\.[0-9]-win64\.exe' | head --lines=1 > ./tmp-aed/.exec-work
 sed --in-place '1 i\https:' ./tmp-aed/.exec-work
 sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
+# download exec
 wget --quiet --show-progress --input-file=./tmp-aed/.exec-work --directory-prefix=./tmp-aed/
-rm ./tmp-aed/.exec-work
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/VLC " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://www.vlc.de/vlc_download_64bit.php | grep --extended-regexp --only-matching --ignore-case 'Media Player V[0-9]\.[0-9]\.[0-9]{1,2}' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/vlc-*-win64.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### mp3tag ###
-echo -e "\e[0m\n<$(date +"%T")> Getting MP3Tag...\e[90m"
+## download ##
+echo -e "\e[0m\n<$(date +"%T")> Getting Mp3tag...\e[90m"
+# download exec
 wget --quiet --output-document=- https://www.mp3tag.de/dodownload.html | sed --expression=':a;N;$!ba;s/\s\{2\}\n//g' | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching 'https://download\.mp3tag\.de/mp3tagv[0-9]{3}setup\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/Mp3tag " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://www.mp3tag.de/en/download.html | grep --extended-regexp --only-matching --ignore-case 'Mp3tag v[0-9]\.[0-9]{1,2}' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/mp3tagv*setup.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### java ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting Java (JRE)...\e[90m"
+# download exec
 wget --quiet --output-document=- https://java.com/de/download/manual.jsp | grep --extended-regexp --only-matching --ignore-case '<a title="Download der Java-Software für Windows \(64-Bit\)" href="[^\"]+"' | grep --extended-regexp --only-matching 'https://javadl\.oracle\.com/webapps/download/AutoDL\?BundleId=[0-9]{6}_([0-9]|[a-z]){32}' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
-echo "./tmp-aed/JRE " > ./tmp-aed/.exec-work
-wget --quiet --output-document=- https://java.com/de/download/manual.jsp | grep --extended-regexp --only-matching --ignore-case 'Version [0-9] Update [0-9]{3}' | head --lines=1 >> ./tmp-aed/.exec-work
-echo ".exe" >> ./tmp-aed/.exec-work
-sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
-cat ./tmp-aed/.exec-work | xargs --replace={} mv ./tmp-aed/AutoDL\?BundleId\=* {}
-rm ./tmp-aed/.exec-work
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/JRE " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://java.com/de/download/manual.jsp | grep --extended-regexp --only-matching --ignore-case 'Version [0-9] Update [0-9]{1,3}' | sed --regexp-extended 's/Version //;s/ Update /\./' | head --lines=1 >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/AutoDL\?BundleId\=* {}
+rm ./tmp-aed/.exec-*
 
 
 ### notepad++ ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting Notepad++...\e[90m"
+# get link to download page
 wget --quiet --output-document=- https://notepad-plus-plus.org/download | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '/repository/[0-9]\.x/[0-9]\.[0-9]\.[0-9]/npp\.[0-9]\.[0-9]\.[0-9]\.Installer\.x64\.exe' | head --lines=1 > ./tmp-aed/.exec-work
 sed --in-place '1 i\https://notepad-plus-plus.org' ./tmp-aed/.exec-work
 sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
+# download exec
 wget --quiet --show-progress --input-file=./tmp-aed/.exec-work --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
 rm ./tmp-aed/.exec-work
+
+## rename ##
+echo "./tmp-aed/Notepad++ " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://notepad-plus-plus.org/download | grep --extended-regexp --only-matching --ignore-case 'Download Notepad\+\+ [0-9]\.[0-9]\.[0-9]' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]\.[0-9]' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/npp.*.Installer.x64.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### balena etcher ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting Balena Etcher...\e[90m"
-wget --quiet --output-document=- https://www.balena.io/etcher/ | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching 'https://github\.com/balena-io/etcher/releases/download/v[0-9]\.[0-9]\.[0-9]{2}/balenaEtcher-Setup-[0-9]\.[0-9]\.[0-9]{2}-x64\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+# get link to download page
+wget --quiet --output-document=- https://github.com/balena-io/etcher/releases | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching '/balena-io/etcher/releases/download/v[0-9]\.[0-9]\.[0-9]{1,2}/balenaEtcher-Setup-[0-9]\.[0-9]\.[0-9]{1,2}-x64\.exe' | head --lines=1 > ./tmp-aed/.exec-work
+sed --in-place '1 i\https://github\.com' ./tmp-aed/.exec-work
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-work
+# download exec
+wget --quiet --show-progress --input-file=./tmp-aed/.exec-work --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/balenaEtcher " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://github.com/balena-io/etcher/releases | grep --extended-regexp --only-matching --ignore-case 'v[0-9]\.[0-9]\.[0-9]{1,2}' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]\.[0-9]{1,2}' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/balenaEtcher-Setup-*-x64.exe {}
+rm ./tmp-aed/.exec-*
 
 
-### teamspeak client ###
-echo -e "\e[0m\n<$(date +"%T")> Getting Teamspeak Client...\e[90m"
+### teamspeak 3 client ###
+## download ##
+echo -e "\e[0m\n<$(date +"%T")> Getting Teamspeak 3 Client...\e[90m"
+# download exec
 wget --quiet --output-document=- https://www.teamspeak.de/download/teamspeak-3-64-Bit-client-windows/ | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching 'http://dl\.4players\.de/ts/releases/[0-9]\.[0-9]\.[0-9]/TeamSpeak3-Client-win64-[0-9]\.[0-9]\.[0-9]\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
+
+## rename ##
+echo "./tmp-aed/Teamspeak 3 Client " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- https://www.teamspeak.de/download/teamspeak-3-64-Bit-client-windows/ | grep --extended-regexp --only-matching --ignore-case 'TS3 Win64 Client [0-9]\.[0-9]\.[0-9]' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]\.[0-9]' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/TeamSpeak3-Client-win64-*.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### filezilla ###
+## download ##
 echo -e "\e[0m\n<$(date +"%T")> Getting FileZilla...\e[90m"
+# download exec
 wget --quiet --output-document=- --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://filezilla-project.org/download.php?type=client | grep --extended-regexp --only-matching --ignore-case '<a.+href="[^\"]+"' | grep --extended-regexp --only-matching 'https://download\.filezilla-project\.org/client/FileZilla_[0-9]\.[0-9]{2}\.[0-9]_win64_sponsored-setup\.exe' | head --lines=1 | xargs wget --quiet --show-progress --directory-prefix=./tmp-aed/
+echo -en "\e[0m"
 
+## rename ##
+echo "./tmp-aed/FileZilla  " > ./tmp-aed/.exec-rename
+wget --quiet --output-document=- --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)" https://filezilla-project.org/download.php?type=client | grep --extended-regexp --only-matching --ignore-case 'FileZilla Client is [0-9]\.[0-9]{1,2}\.[0-9]' | head --lines=1 | grep --extended-regexp --only-matching '[0-9]\.[0-9]{1,2}\.[0-9]' >> ./tmp-aed/.exec-rename
+echo ".exe" >> ./tmp-aed/.exec-rename
+sed --in-place ':a;N;$!ba;s/\n//g' ./tmp-aed/.exec-rename
+cat ./tmp-aed/.exec-rename | xargs --replace={} mv ./tmp-aed/FileZilla_*_win64_sponsored-setup.exe {}
+rm ./tmp-aed/.exec-*
 
 
 ### TODO ###
@@ -164,7 +294,7 @@ wget --quiet --output-document=- --user-agent="Mozilla/5.0 (Windows NT 10.0; Win
 
 ##### FINISHING #####
 ### making log ###
-echo -e "\e[0m"
+echo -en "\e[0m"
 echo -e "\e[92mSuccessfully downloaded $(find ./tmp-aed/*.{exe,msi} | wc --lines)/12 programms\e[0m" | tee ./tmp-aed/aed-$(date +%Y.%m.%d-%H-%M-%S).log
 find ./tmp-aed/*.{exe,msi} >> ./tmp-aed/aed-$(date +%Y.%m.%d-%H-%M-%S).log
 sed --in-place 's/tmp-aed\///' ./tmp-aed/*.log
